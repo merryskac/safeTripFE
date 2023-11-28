5c619129ee4a469feeecbd35bfcc7a7484293078
unknown <merryskachr@gmail.com>
2023-11-28 16:10:26 +0800

    vercel

--- fe/watch-a-trip/src/components/axios/api.js
+++ fe/watch-a-trip/src/components/axios/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: 'http://172.20.10.3:3000/api',
+	baseURL: 'https://pls-phi.vercel.app/api',
 	// baseURL: 'http://localhost:3000/api',
 	headers: {
 		'Content-Type': 'application/json'
--- fe/watch-a-trip/src/components/socket/socketInit.js
+++ fe/watch-a-trip/src/components/socket/socketInit.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-export const socket = io('http://172.20.10.3:3000');
+export const socket = io('https://pls-phi.vercel.app');
 // export const socket = io('http://localhost:3000');
