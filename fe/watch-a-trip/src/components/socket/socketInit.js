ad505ca202c7ed8c47f788d2866295fef1a5c8eb
unknown <merryskachr@gmail.com>
2023-11-28 16:40:46 +0800

    vercel

--- fe/watch-a-trip/src/components/socket/socketInit.js
+++ fe/watch-a-trip/src/components/socket/socketInit.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-export const socket = io('wss://pls-phi.vercel.app');
+export const socket = io('https://pls-phi.vercel.app');
 // export const socket = io('http://localhost:3000');
